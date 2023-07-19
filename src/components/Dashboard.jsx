564975a5abfcb7c8eb038ec452bc01773a2ab323
MerinSusanJacob <129526220+MerinSusanJacob@users.noreply.github.com>
2023-07-19 22:29:01 +0930

    Initial commit

---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Home';
+
+const Dashboard = () => {
+  return (
+    <div>
+        <Home/>
+        <h1>Dashboard</h1>
+    </div>
+  )
+}
+
+export default Dashboard
---
+++ src/components/Form.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './Home';
+
+const Form = () => {
+  return (
+    <div>
+        <Home/>
+        <h1>Form</h1>
+    </div>
+  )
+}
+
+export default Form
